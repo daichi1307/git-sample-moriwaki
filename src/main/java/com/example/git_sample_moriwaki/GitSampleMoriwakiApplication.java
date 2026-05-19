ff4c0d0612e579b658180fcd56757f7feaac4aa5
daichi1307 <m.daichi4613@gmail.com>
2026-05-19 11:45:06 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/git_sample_moriwaki/GitSampleMoriwakiApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_sample_moriwaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleMoriwakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleMoriwakiApplication.class, args);
+	}
+
+}
